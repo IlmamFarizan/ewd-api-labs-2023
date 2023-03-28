d1161b9083fbe24cc111f6a4ba0d40855b0b06c4
IlmamFarizan <42376326+IlmamFarizan@users.noreply.github.com>
2023-03-28 14:43:55 +0000

    Update Movies Endpoints

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './src/movies';
+// import moviesRouter from './src/movies';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from "./src/config/dependencies";
+import createMoviesRouter from './src/movies/routes';
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ const dependencies = buildDependencies();
 
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
+// app.use('/api/movies', moviesRouter);
+app.use('/api/movies', createMoviesRouter(dependencies));
 
 app.use('/api/accounts', createAccountsRouter(dependencies));
 
